ea04a24ced30ae705d74879b51a7c4c78e0b23b9
T10a <00001082945705sp@al.educacao.sp.gov.br>
2024-10-29 11:13:53 -0300

    03

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Angola?',
+    'A capital da Angola é Luanda'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que significa nigga em Inglês?',
+    'Significa #####'
+)
